da44dc2d562e0386c6879fd0f6485e4679f71b2c
KOYN <gkeo933@gmail.comgit config --global user.email gkeo933@gmail.comgit config --global user.email gkeo933@gmail.com>
2025-06-23 23:13:28 +0300

    Printing each character of the word Factory with specific pattern

---
+++ challenges/challenge1.py
@@ -0,0 +1,22 @@
+s = "Factory"
+
+s1 = s[0]
+s2 = s[1] * 2
+s3 = s[2] * 3
+s4 = s[3] * 4
+s5 = s[4] * 5
+s6 = s[-2] * 6
+s7 = s[-1] * 7
+ 
+ 
+ 
+# print(s1)
+# print(s2)
+# print(s3)
+# print(s4)
+# print(s5)
+# print(s6)
+# print(s7)
+
+#F-string interpolation
+print(f"{s1}\n"+ f"{s2}\n"+ f"{s3}\n"+ f"{s4}\n"+ f"{s5}\n"+ f"{s6}\n"+ f"{s7}")
